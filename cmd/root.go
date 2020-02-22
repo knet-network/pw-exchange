cb543cf442f19c54d99dae7d2746b1eed572f3eb
Sven Klemmer <svenklemmer@t-online.de>
2020-02-22 22:14:27 +0100

    looks fine

--- cmd/root.go
+++ cmd/root.go
@@ -11,8 +11,7 @@ import (
 
 var (
 	// Used for flags.
-	cfgFile     string
-	userLicense string
+	cfgFile string
 
 	rootCmd = &cobra.Command{
 		Use:   "cobra",
@@ -32,16 +31,6 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
-	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
-	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
-	viper.SetDefault("license", "apache")
-
-	//rootCmd.AddCommand(addCmd)
-	//rootCmd.AddCommand(initCmd)
 }
 
 func er(msg interface{}) {
--- cmd/server.go
+++ cmd/server.go
@@ -2,11 +2,23 @@ package cmd
 
 import (
 	"github.com/aerogo/aero"
+	"github.com/spf13/cobra"
 )
 
-func main() {
+func init() {
+	rootCmd.AddCommand(serverCmd)
+}
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start PW Exchange Serveer",
+	RunE:  serve,
+}
+
+func serve(cmd *cobra.Command, args []string) error {
 	app := aero.New()
 	configure(app).Run()
+	return nil
 }
 
 func configure(app *aero.Application) *aero.Application {
